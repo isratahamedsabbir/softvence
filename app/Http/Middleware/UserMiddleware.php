4161e1df06723ee8cfc8f57489a57fae2b650e8a
isratahamedsabbi <isratahamedsabbir@gmail.com>
2025-03-27 00:01:06 +0600

    u

--- app/Http/Middleware/UserMiddleware.php
+++ app/Http/Middleware/UserMiddleware.php
@@ -10,7 +10,7 @@ class UserMiddleware
 {
     public function handle(Request $request, Closure $next)
     {
-        if (Auth::check() && Auth::guard('api')->check() && Auth::user()->hasRole('user')) {
+        if (Auth::guard('api')->check() && Auth::guard('api')->check() && Auth::guard('api')->user()->hasRole('user')) {
             return $next($request);
         }
 
