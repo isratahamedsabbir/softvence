53717f83702b1c7246c2d7aae22f49985f9d9726
isratahamedsabbir <isratahanedsabbir@gmail.com>
2025-03-30 12:35:52 +0600

    u

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,10 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    public function __construct()
+    {
+        $locale = session()->get('locale') ?? "en";
+        Session::put('locale', $locale);
+        App::setLocale($locale);
+    }
 }
